c12d22c52a61432544425678f868b7fea6af00d0
Mr-friendly-Spy <153740346+Mr-friendly-Spy@users.noreply.github.com>
2025-10-05 16:22:17 +0300

    Add files via upload

---
+++ informatica/practice_4/homework/ConsoleApplication3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	float C;
+	float number = 1.0;
+	float x;
+	float y;
+	float R;
+	float S;
+	cin >> x;
+	cin >> y;
+	R = sqrt(pow(x, 2) + pow(y, 2)) * sin(x);
+	S = pow(exp(number), x / y);
+	cout << R << " " << S << endl;
+	C = max(R, S);
+	cout << C << endl;
+	system("pause");
+	return 0;
+}
+
+
