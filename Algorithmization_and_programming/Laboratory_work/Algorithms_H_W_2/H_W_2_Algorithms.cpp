c25805c960e412cba3e663a44df93fd4de94f857
Mr-friendly-Spy <153740346+Mr-friendly-Spy@users.noreply.github.com>
2026-02-22 20:07:48 +0300

    add new h/w_2 data
    add new h/w_2 data 
    22.02.26

---
+++ Algorithmization_and_programming/Laboratory_work/Algorithms_H_W_2/H_W_2_Algorithms.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// Функции для верхней и нижней границ
+double upper(double x) {
+    return x * x + 4 * x;
+}
+
+double lower(double x) {
+    return -x * x - 4 * x;
+}
+
+// Метод Монте-Карло для площади между кривыми
+double monte_carlo_area(double x_min, double x_max, double y_min, double y_max, int num_points) {
+    int points_inside = 0;
+
+    for (int i = 0; i < num_points; ++i) {
+        // Случайная точка в прямоугольнике
+        double x = x_min + (rand() / (RAND_MAX + 1.0)) * (x_max - x_min);
+        double y = y_min + (rand() / (RAND_MAX + 1.0)) * (y_max - y_min);
+
+        // Проверяем, попала ли точка внутрь фигуры
+        if (y >= lower(x) && y <= upper(x)) {
+            points_inside++;
+        }
+    }
+
+    double rect_area = (x_max - x_min) * (y_max - y_min); // площадь прямоугольника
+    return (points_inside / static_cast<double>(num_points)) * rect_area; 
+}
+
+int main() {
+    srand(time(0));
+
+    double x_min = 3.0, x_max = 4.0;
+    double y_min = -32.0, y_max = 32.0; //Вручную найдём экстремумы на отрезке [3, 4]
+    int num_points = 1000000;
+
+    double area = monte_carlo_area(x_min, x_max, y_min, y_max, num_points);
+    cout << "Approximate area of the shape: " << area << endl;
+
+    return 0;
+}
---
+++ Algorithmization_and_programming/Laboratory_work/Algorithms_H_W_2/UnitTest_H_W_2_Algorithms.cpp
@@ -0,0 +1,53 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../H_W_2_Algorithms/H_W_2_Algorithms.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestHW2Algorithms
+{
+	TEST_CLASS(UnitTestHW2Algorithms)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+            srand(time(0)); // Для повторяемости
+            double x_min = 3.0, x_max = 4.0;
+            double y_min = -32.0, y_max = 32.0;
+            int num_points = 100000;
+
+            // Запускаем дважды
+            double result1 = monte_carlo_area(x_min, x_max, y_min, y_max, num_points);
+            double result2 = monte_carlo_area(x_min, x_max, y_min, y_max, num_points);
+
+            // Должны получить одинаковые результаты
+            double epsilon1 = 0.1;
+            Assert::AreEqual(result1, result2, epsilon1, L"Значения не совпали с данной точностью");
+		}
+
+        TEST_METHOD(TestMethod2)
+        {
+            double x_min = 3.0, x_max = 4.0;
+            double y_min = -32.0, y_max = 32.0;
+            int num_points = 10000;
+
+            double rect_area = (x_max - x_min) * (y_max - y_min); // 1 * 64 = 64
+            double result = monte_carlo_area(x_min, x_max, y_min, y_max, num_points);
+
+            // Проверка, что площадь меньше пощади прямоугольника
+            Assert::IsTrue(result < rect_area); 
+        }
+
+        TEST_METHOD(TestMethod3)
+        {
+            double x_min = 3.0, x_max = 4.0;
+            double y_min = -32.0, y_max = 32.0;
+            int num_points = 10000;
+
+            double result = monte_carlo_area(x_min, x_max, y_min, y_max, num_points);
+
+            // Проверка, что площадь фигуры положительна
+            Assert::IsTrue(result > 0);
+        }
+	};
+}
